dcbaa6a179e0a7ad5607cc33b969dfee2bfa743f
JSL107 <juneseok0107@naver.com>
2022-07-12 09:29:12 +0900

    0711과제

---
+++ 0711/aplusb/Main.java
@@ -0,0 +1,17 @@
+package src.aplusb;
+
+import java.util.Scanner;
+
+public class Main {
+    public static int plus(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(plus(a, b));
+    }
+}
