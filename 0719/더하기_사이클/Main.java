9fac3e0947a23b98bab5c8ea698d96c449ccf336
JSL107 <juneseok0107@naver.com>
2022-07-19 10:32:04 +0900

    0719 과제

--- 0719/더하기_사이클/Main.java
+++ 0719/더하기_사이클/Main.java
@@ -4,15 +4,35 @@ package 더하기_사이클;
 // 0 보다 크거나 같고 99보다 작거나 같은 정수
 // 10 보다 작으면 0을 붙여 두자리로 만들고 각 자리를 더한다.
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
+    // 0~99
+    // 1~9 + 0
+    // 그 다음 주어진 숫자 xy 에서 x를 떼서 붙임
+    // x+y = z => z+y = a => y+a => ?
+
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
+
+
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-//        ParseInt
-        list.add(a);
+        int N = sc.nextInt();
+        sc.close();
+
+        int count = 0;
+        int copy = N;
+        while (true) {
+            N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
+            count++;
+
+            if (copy == N) {
+                break;
+            }
+
+        }
+        System.out.println(count);
     }
 }
