3786da2dcc49e6a0fa9fbf0ccdeed351dc2eec88
JSL107 <juneseok0107@naver.com>
2022-07-20 16:25:04 +0900

    0721

--- 0721/셀프_넘버/Main.java
+++ 0721/셀프_넘버/Main.java
@@ -1,4 +1,21 @@
 package 셀프_넘버;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        String[] array = new String[count];
+        for (int i = 0; i < count; i++) {
+            array[i] = sc.next();
+            for (int y = 0; y < array[i].length(); i++) {
+                System.out.print(array[i].charAt(y) + " ");
+            }
+        }
+
+
+    }
+
 }
